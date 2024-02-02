64204c60406cb27cca514ad6e899c02620dc5698
yulia_t <127133635+YuliaTratkanova@users.noreply.github.com>
2024-02-02 10:09:14 +0400

    Add files via upload

---
+++ лаб_2_дод/лаб_2_дод/лаб_2_дод.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <time.h>
+#define n 5
+#define m 3
+int main()
+{
+	int x[n][m];
+	srand(time(NULL));
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			x[i][j] = rand() % 100 - 50; printf("%3d  ", x[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+
+	int i_max = 0, sum_max = 0, min_max = 32000, j_pos;
+	for (int j = 0; j < m; j++) {
+		i_max = x[0][j];
+		for (int i = 0; i < n; i++) {
+			if (i_max < x[i][j]) {
+				i_max = x[i][j];
+			}
+		}
+		printf("%3d ", i_max);
+		sum_max += i_max;
+		if (i_max < min_max) {
+			min_max = i_max;
+			j_pos = j;
+		}
+	}
+	printf("\nsum of max %d\n", sum_max);
+
+	for (int i = 0; i < n; i++) {
+		printf("%3d\n", x[i][j_pos]);
+	}
+}
